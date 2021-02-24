2ceda9691385eb07d16ceb5fff66e7ea261563d1
Priyank Dev Varshney <priyankdevvarshney@Priyanks-MacBook-Pro.local>
2021-02-24 10:10:38 +0530

    rediscommit

--- src/main/java/com/example/SpringPostgres/entity/Employee.java
+++ src/main/java/com/example/SpringPostgres/entity/Employee.java
@@ -1,16 +1,16 @@
 package com.example.SpringPostgres.entity;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
 
-@Document(collection = "febemployee")
+@RedisHash(value = "FebEmployee")
 @Getter
 @Setter
 
 public class Employee {
-    @MongoId
+    @Id
     private Long id;
     private String name;
     private String departmentName;
